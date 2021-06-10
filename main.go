4507e1b957bc39e6117e0e57ab06cff4b56cc2db
White <it007@cht-group.net>
2021-06-10 15:30:48 +0800

    表单验证

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
+	"unicode/utf8"
 )
 
 var router = mux.NewRouter()
@@ -34,19 +37,77 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprint(w, "访问文章列表")
 }
 
+type ArticlesFormData struct {
+	Title, Body string
+	URL *url.URL
+	Errors map[string]string
+}
+
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request)  {
-	err := r.ParseForm()
-	if err != nil {
-		//解析错误
-		fmt.Fprint(w, "请提供正确的数据")
-		return
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+
+	errors := make(map[string]string)
+
+	//验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度为 3-40"
 	}
 
-	title := r.PostForm.Get("title")
+	//验证内容
+	if body == "" {
+		errors["body"] = "内容不可为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度必须大于或等于10个字节"
+	}
 
-	fmt.Fprintf(w, "POST postForm %v <br>", r.PostForm)
-	fmt.Fprintf(w, "post form %v <br>", r.Form)
-	fmt.Fprintf(w, "title value is: %v", title)
+	//检查是否有错误
+	if len(errors) == 0 {
+		fmt.Fprint(w, "通过验证！<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	} else {
+		html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+    <style type="text/css">.error {color: red;}</style>
+</head>
+<body>
+    <form action="{{ .URL }}" method="post">
+        <p><input type="text" name="title" value="{{ .Title }}"></p>
+        {{ with .Errors.title }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+        {{ with .Errors.body }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+		storeURl, _ := router.Get("articles.store").URL()
+
+		data := ArticlesFormData{
+			Title: title,
+			Body: body,
+			URL: storeURl,
+			Errors: errors,
+		}
+		tmpl, err := template.New("crate-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		tmpl.Execute(w, data)
+	}
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler  {
@@ -110,11 +171,11 @@ func main() {
 	//中间件，强制内容为HTMl
 	router.Use(forceHTMLMiddleware)
 	//通过命名路由获取URL示例
-	homeURl, _ := router.Get("home").URL()
-	fmt.Println("homeURL:", homeURl)
-	articleURL, _ := router.Get("articles.show").
-		URL("id", "23")
-	fmt.Println("articleURL", articleURL)
+	//homeURl, _ := router.Get("home").URL()
+	//fmt.Println("homeURL:", homeURl)
+	//articleURL, _ := router.Get("articles.show").
+	//	URL("id", "23")
+	//fmt.Println("articleURL", articleURL)
 
 	http.ListenAndServe(":8005", removeTrailingSlash(router))
 }
