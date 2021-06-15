bc1b37d9eaafb91b016d63b632bc34e0d08b4ec9
White <it007@cht-group.net>
2021-06-15 17:19:45 +0800

    退出登录

--- routes/web.go
+++ routes/web.go
@@ -42,6 +42,7 @@ func RegisterWebRoutes(r *mux.Router)  {
 
 	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
 	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
+	r.HandleFunc("/auth/logout", auc.Logout).Methods("POST").Name("auth.logout")
 
 	//静态资源
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
